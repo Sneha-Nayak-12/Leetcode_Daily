1fc9ab901550fc89995d17254cd295b0f4622a54
Sneha Priyadarsini Nayak <snehapriyadarsininayak@gmail.com>
2026-07-09 01:45:21 +0530

    Time: 0 ms (100%), Space: 9.7 MB (76.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+            return false;
         int freq[26] = {0};
         for(int i=0;i<s.length();i++)
         {
