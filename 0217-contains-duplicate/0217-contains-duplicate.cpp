506ed935c1881aea45c162df6dd90c1989391c96
Sneha Priyadarsini Nayak <snehapriyadarsininayak@gmail.com>
2026-07-19 23:29:07 +0530

    Time: 75 ms (24.54%), Space: 111.2 MB (24.2%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-
-        // unordered_set<int> s;
-
-        // for(int num : nums)
-        // {
-        //     if(s.count(num))
-        //     {
-        //         return true;
-        //     }
-
-        //     s.insert(num);
-        // }
-
-        // return false;
         unordered_set<int> s;
         for(int i=0;i<nums.size();i++)
         {
@@ -22,10 +8,7 @@ public:
             {
                 return true;
             }
-            else
-            {
-                s.insert(nums[i]);
-            }
+            s.insert(nums[i]);
         }
         return false;
     }
