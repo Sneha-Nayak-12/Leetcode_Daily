fe6336055052ce18381b87f064ad7424a43aa915
Sneha Priyadarsini Nayak <snehapriyadarsininayak@gmail.com>
2026-07-23 02:45:30 +0530

    Time: 0 ms (100%), Space: 23.9 MB (56.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left = 0;
-        for(int right=0;right<nums.size();right++)
+        // int left = 0;
+        // for(int right=0;right<nums.size();right++)
+        // {
+        //     if(nums[right]!=0)
+        //     {
+        //         swap(nums[left],nums[right]);
+        //         left++;
+        //     }
+        // }
+
+        int l =0;
+        for(int r=0;r<nums.size();r++)
         {
-            if(nums[right]!=0)
+            if(nums[r] != 0)
             {
-                swap(nums[left],nums[right]);
-                left++;
+                swap(nums[l],nums[r]);
+                l++;
             }
         }
         return;
